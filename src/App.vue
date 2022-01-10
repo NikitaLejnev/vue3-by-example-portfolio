aed6fe6d16eb89faea586d7d3ac0e2e2aa6db185
NikitaLejnev <catassasin331@gmail.com>
2022-01-10 14:50:14 +0300

    Added code to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<template></template>
+<template>
+  <div>
+    <h1>Github App</h1>
+    <GitHubTokenForm />
+    <User />
+    <Repos />
+  </div>
+</template>
 
 <script>
+import GitHubTokenForm from "./components/GitHubTokenForm.vue";
+import User from "./components/User.vue";
+import Repos from "./components/Repos.vue";
+
 export default {
   name: 'App',
-  components: {}
-}
+  components: { GitHubTokenForm, User, Repos }
+};
 </script>
